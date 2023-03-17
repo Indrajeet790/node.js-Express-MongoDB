e7508adb13cb84c28829c5ba6d107d6c62d9d07d
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-03-17 18:53:35 +0530

    asynchrous/add txt file/add image file

--- Asynchronous-js/index.js
+++ Asynchronous-js/index.js
@@ -1,5 +1,15 @@
-const fs = require("fs");
+const fs = require('fs');
 
-fs.readFile(`${_dirname}/dog.txt`, (err, data) => {
-  console.log("Breed:`${data}");
+const superAgent = require('superagent');
+
+fs.readFile(`Asynchronous-js/dog.txt`, (err, data) => {
+  console.log(`Breed:${data}`);
+
+  superAgent.get(`https://dog.ceo/api/breeds/image/random`).end((err, resp) => {
+    console.log(resp?.body?.message);
+
+    fs.writeFile('Asynchronous-js/dog-img.txt', resp?.body?.message, (err) => {
+      console.log('random dog image save to file');
+    });
+  });
 });
