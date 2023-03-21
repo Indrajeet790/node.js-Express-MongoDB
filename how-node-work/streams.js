0c74194fd040226fd09647c586e1247ee3066e62
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-03-21 09:26:24 +0530

    read file eith the help of pipe() operator

--- how-node-work/streams.js
+++ how-node-work/streams.js
@@ -9,21 +9,26 @@ server.on("request", (req, resp) => {
   //   }
   //   resp.end(data);
   // });
-
   //Solution 2:streams
+  // const readable = fs.createReadStream("./how-node-work/test-file.txt");
+  // readable.on("data", (chunk) => {
+  //   resp.write(chunk);
+  // });
+  // // reading data from file
+  // readable.on("end", () => {
+  //   resp.end();
+  // });
+  // readable.on("error", (err) => {
+  //   console.log("err");
+  //   resp.statusCode = 500;
+  //   resp.end("file is not found");
+  // });
+
+  // solution 3 use pipe()operator
+
   const readable = fs.createReadStream("./how-node-work/test-file.txt");
-  readable.on("data", (chunk) => {
-    resp.write(chunk);
-  });
-  // reading data from file
-  readable.on("end", () => {
-    resp.end();
-  });
-  readable.on("error", (err) => {
-    console.log("err");
-    resp.statusCode = 500;
-    resp.end("file is not found");
-  });
+  readable.pipe(resp);
+  // readableSource.pipe(writeableDest)
 });
 server.listen(8000, "127.0.0.1", () => {
   console.log("server is running");
