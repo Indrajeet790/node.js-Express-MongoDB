726db4ece7914a495317d67f372e51df62c409a7
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-03-21 10:16:55 +0530

    creating a new test-modules3 and export into in modules.js //caching method

--- how-node-work/modules.js
+++ how-node-work/modules.js
@@ -7,5 +7,12 @@ const calc1 = new Cal();
 console.log(calc1.add(2, 5));
 
 // exports
-const calc2 = require("./test-module2");
-console.log(calc2.multiply(3, 7));
+// const calc2 = require("./test-module2");
+// console.log(calc2.multiply(3, 7));
+
+//
+const { add, multiply, divide } = require("./test-module2");
+console.log(multiply(6, 2));
+
+// caching
+require("./test-modules3")();
---
+++ how-node-work/test-modules3.js
@@ -0,0 +1,5 @@
+console.log("hello from the modules");
+
+module.exports = () => {
+  console.log("Log this beautiful text👨‍🦱");
+};
