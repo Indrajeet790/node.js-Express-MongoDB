88ec2e5a612ca554e8c68a69d7c1b2d470fc6664
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-03-21 09:21:38 +0530

    read file eith the help or readable streams

--- how-node-work/streams.js
+++ how-node-work/streams.js
@@ -2,17 +2,29 @@ const fs = require("fs");
 const server = require("http").createServer();
 
 server.on("request", (req, resp) => {
-  // Solution
-  fs.readFile("./how-node-work/test-file.txt", (err, data) => {
-    if (err) {
-      console.log("err");
-    }
-    // else {
-    //   console.log("success");
-    // }
-    resp.end(data);
+  // Solution1
+  // fs.readFile("./how-node-work/test-file.txt", (err, data) => {
+  //   if (err) {
+  //     console.log("err");
+  //   }
+  //   resp.end(data);
+  // });
+
+  //Solution 2:streams
+  const readable = fs.createReadStream("./how-node-work/test-file.txt");
+  readable.on("data", (chunk) => {
+    resp.write(chunk);
+  });
+  // reading data from file
+  readable.on("end", () => {
+    resp.end();
+  });
+  readable.on("error", (err) => {
+    console.log("err");
+    resp.statusCode = 500;
+    resp.end("file is not found");
   });
 });
 server.listen(8000, "127.0.0.1", () => {
-  console.log("Listening.....");
+  console.log("server is running");
 });
